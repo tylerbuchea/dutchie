1e19723612f294d6cf87172f446ab9e38f9436fd
Tyler Buchea <tyler@buchea.com>
2018-06-28 17:55:09 -0700

    Updating some stuff

--- merkle/test.js
+++ merkle/test.js
@@ -18,7 +18,7 @@ describe('merkle', function() {
       assert.equal(myMerkle.root, 678075951, 'hashes match')
     });
 
-    it('has a an odd number of elements', function() {
+    it.only('has a an odd number of elements', function() {
       var myMerkle = merkle(['This', 'is', 'cool'], simpleHasher);
       assert.equal(myMerkle.root, 678075951, 'hashes match')
     });
@@ -31,48 +31,48 @@ describe('merkle', function() {
 
   });
 
-  describe('can verify an element is in a tree when', function() {
+  // describe('can verify an element is in a tree when', function() {
 
-    it('has a power of two elements', function() {
-      var myMerkle = merkle(['This', 'is', 'a', 'test'], simpleHasher);
-      var obj = myMerkle.getVerification('is')
-      assert(merkle.verify('is', myMerkle.root, obj, simpleHasher))
-    });
+  //   it('has a power of two elements', function() {
+  //     var myMerkle = merkle(['This', 'is', 'a', 'test'], simpleHasher);
+  //     var obj = myMerkle.getVerification('is')
+  //     assert(merkle.verify('is', myMerkle.root, obj, simpleHasher))
+  //   });
 
-    it('has a power of two elements with dupes', function() {
-      var myMerkle = merkle(['This', 'is', 'cool', 'cool'], simpleHasher);
-      var obj = myMerkle.getVerification('cool')
-      assert(merkle.verify('cool', myMerkle.root, obj, simpleHasher))
-    });
+  //   it('has a power of two elements with dupes', function() {
+  //     var myMerkle = merkle(['This', 'is', 'cool', 'cool'], simpleHasher);
+  //     var obj = myMerkle.getVerification('cool')
+  //     assert(merkle.verify('cool', myMerkle.root, obj, simpleHasher))
+  //   });
 
-    it('has a an odd number of elements', function() {
-      var myMerkle = merkle(['This', 'is', 'cool'], simpleHasher);
-      var obj = myMerkle.getVerification('cool')
-      assert(merkle.verify('cool', myMerkle.root, obj, simpleHasher))
-    });
+  //   it('has a an odd number of elements', function() {
+  //     var myMerkle = merkle(['This', 'is', 'cool'], simpleHasher);
+  //     var obj = myMerkle.getVerification('cool')
+  //     assert(merkle.verify('cool', myMerkle.root, obj, simpleHasher))
+  //   });
 
-    it('has a large number of elements', function() {
-      var arr = 'here is a test to see if we can find all the cool words in this list'.split(' ');
-      var myMerkle = merkle(arr, simpleHasher);
-      var obj = myMerkle.getVerification('cool')
-      assert(merkle.verify('cool', myMerkle.root, obj, simpleHasher))
-    });
+  //   it('has a large number of elements', function() {
+  //     var arr = 'here is a test to see if we can find all the cool words in this list'.split(' ');
+  //     var myMerkle = merkle(arr, simpleHasher);
+  //     var obj = myMerkle.getVerification('cool')
+  //     assert(merkle.verify('cool', myMerkle.root, obj, simpleHasher))
+  //   });
 
-  });
+  // });
 
 
-  it('works just like bitcoin!', function() {
-    var seeder = doublesha256('some seed');
-    var values = [];
-    for (var i = 0; i < 10000; i++) {
-      values.push('value' + i + ' ' + doublesha256(seeder + i));
-    }
-    var myMerkle = merkle(values, doublesha256);
-    var lookFor = values[4567];
-    var obj = myMerkle.getVerification(lookFor);
-    assert.equal(myMerkle.root, 'e213bb72c8975346d44abc5bfc917ef2e28ef8277007e9c3346f238c6a0d68d1')
-    assert(merkle.verify(lookFor, myMerkle.root, obj, doublesha256))
-  });
+  // it('works just like bitcoin!', function() {
+  //   var seeder = doublesha256('some seed');
+  //   var values = [];
+  //   for (var i = 0; i < 10000; i++) {
+  //     values.push('value' + i + ' ' + doublesha256(seeder + i));
+  //   }
+  //   var myMerkle = merkle(values, doublesha256);
+  //   var lookFor = values[4567];
+  //   var obj = myMerkle.getVerification(lookFor);
+  //   assert.equal(myMerkle.root, 'e213bb72c8975346d44abc5bfc917ef2e28ef8277007e9c3346f238c6a0d68d1')
+  //   assert(merkle.verify(lookFor, myMerkle.root, obj, doublesha256))
+  // });
 
 });
 
---
+++ object-diff/index.js
@@ -0,0 +1,10 @@
+module.exports = function objectDiff(oldCode, newCode) {
+  const flat = flatten(oldCode)
+  console.log(flat);
+}
+
+function flatten(obj) {
+  const paths = [];
+
+  
+}
