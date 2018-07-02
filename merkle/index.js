9a11980cea22cbb5650dcab0d96853fb54aeac1a
Tyler Buchea <tyler@buchea.com>
2018-07-02 11:20:31 -0700

    Cleaning up

--- merkle/index.js
+++ merkle/index.js
@@ -1,17 +1,15 @@
 
 module.exports = function merkle(tree, simpleHasher) {
   // const root = crawlTree(tree, simpleHasher);
-  // console.log(root);
 
   this.verify = function() {
     // console.log('verify');
   }
-  // console.log(tree);
-  // const root = simpleHasher(simpleHasher(tree[0] + tree[1]) + simpleHasher(tree[2] + tree[3]))
-  // const root = simpleHasher(simpleHasher(tree[0] + tree[1]) + tree[2]);
+
   const root = simpleHasher(
     simpleHasher(tree[0] + tree[1]) + tree[2]
   );
+
   console.log(root);
   console.log(678075951);
   
@@ -30,15 +28,9 @@ function crawlTree(tree, simpleHasher) {
   for (let i=0; i<tree.length; i++) {
     const mom = tree.shift();
     const dad = tree.shift();
-    console.log(mom, dad, tree);
     nextTeir.push(simpleHasher(mom + dad));
-    // console.log(nextTeir);
     if (tree.length === 1) nextTeir.push(tree[0]);
-    // if (tree.length === 1) nextTeir.push(simpleHasher(tree[0]));
-    // console.log(nextTeir);
   }
-
-  // console.log(nextTeir.length);
 
   if (nextTeir.length === 1) {
     return nextTeir[0];
